c932b9dc5c9dcb63ec1fd5060fcfb7feaa121f91
Dylan Meissner <dylanmei@gmail.com>
2016-06-11 08:49:26 -0700

    Force term for ssh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias ..="ENHANCD_DISABLE_DOT=1 cd .."
 alias -- -="ENHANCD_DISABLE_HYPHEN=1 cd -"
 alias ll="ls -lAFh --group-directories-first"
 alias tree="tree -C"
+alias ssh="TERM=rxvt-256color ssh"
 
 alias du='du -h'
 alias job='jobs -l'
