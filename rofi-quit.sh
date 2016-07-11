a65f274c94623e82b1e761037881074c605977df
Dylan Meissner <dylanmei@gmail.com>
2016-07-10 17:02:07 -0700

    Update quit script

--- rofi-quit.sh
+++ rofi-quit.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-res=$(echo -e "bspc quit\nreboot\nshutdown\nsuspend\nhibernate" | rofi -dmenu -p "quit: " -l 5)
+res=$(echo -e "exit bspwm\nreboot\nshut down\nsuspend\nhibernate" | rofi -dmenu -p "quit:" -l 5)
 if [ $? -eq 0 ]; then
   case "$res" in
-    "bspc quit")
-      #i3-msg exit ;;
+    "exit bspwm")
       pkill -x panel
       bspc quit
       ;;
-    "shutdown")
+    "shut down")
       systemctl "poweroff" ;;
     *)
       systemctl "$res" ;;
