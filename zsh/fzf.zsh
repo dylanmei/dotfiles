f884b1ae5698856eae78d659713c025f968bd60d
Dylan Meissner <dylanmei@gmail.com>
2016-08-05 21:24:04 -0700

    Fixup fzf ** completion

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,17 +1,2 @@
-if [[ -f /usr/share/fzf/key-bindings.zsh ]]; then
-  _fzf_compgen_path() {
-    echo "$1"
-    \find -L "$1" \
-      -name .git -prune -o -name .svn -prune -o \( -type d -o -type f -o -type l \) \
-      -a -not -path "$1" -print 2> /dev/null | sed 's@^\./@@'
-  }
-
-  _fzf_compgen_dir() {
-    \find -L "$1" \
-      -name .git -prune -o -name .svn -prune -o -type d \
-      -a -not -path "$1" -print 2> /dev/null | sed 's@^\./@@'
-  }
-
-  source /usr/share/fzf/key-bindings.zsh
-fi
-
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
