295569d5a76c504a99edc5ea747e829f8439943b
Dylan Meissner <dylanmei@gmail.com>
2016-12-23 06:27:17 -0800

    Format prompt command script

--- bash/workdir.bash
+++ bash/workdir.bash
@@ -8,18 +8,17 @@ typeset -g SHELL_WORKDIR
 cache_file="/tmp/workdir"
 
 function remember_lwd() {
-	pwd >| "$cache_file"
+  pwd >| "$cache_file"
 }
 
 function jump_to_lwd() {
-	[[ ! -r "$cache_file" ]] || cd "`cat "$cache_file"`"
+  [[ ! -r "$cache_file" ]] || cd "`cat "$cache_file"`"
 }
 
 # Automatically jump to last working directory unless this
 # isn't the first time this plugin has been loaded.
 if [[ -z "$SHELL_WORKDIR" ]]; then
-  echo "here"
-	jump_to_lwd 2>/dev/null && SHELL_WORKDIR=1 || true
+  jump_to_lwd 2>/dev/null && SHELL_WORKDIR=1 || true
 fi
 
 PROMPT_COMMAND="remember_lwd; $PROMPT_COMMAND"
