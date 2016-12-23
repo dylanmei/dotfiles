9411ec4c16e94b7981d313cff41dc78a36877c43
Dylan Meissner <dylanmei@gmail.com>
2016-12-23 06:22:56 -0800

    Rework enhancd setup

--- bash/enhancd.bash
+++ bash/enhancd.bash
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-[[ -d /usr/src/enhancd ]] && export ENHANCD_ROOT=/usr/src/enhancd
-[[ -d ~/src/enhancd ]]    && export ENHANCD_ROOT=~/src/enhancd
+[[ -d /usr/src/enhancd ]]  && export ENHANCD_ROOT=/usr/src/enhancd
+[[ -d $HOME/src/enhancd ]] && export ENHANCD_ROOT=$HOME/src/enhancd
 
 if [[ -n "$ENHANCD_ROOT" ]]; then
   export ENHANCD_FILTER="fzf"
-  export ENHANCD_DOT_ARG=_dot
-  export ENHANCD_HYPHEN_ARG=_hyphen
 
   source $ENHANCD_ROOT/init.sh
 
-  alias ~='cd ~'
-  alias ..='cd _dot'
-  alias -- -='cd _hyphen'
+  alias ~='ENHANCD_DISABLE_HOME=1 cd ~'
+  alias ..='ENHANCD_DISABLE_DOT=1 cd ..'
+  alias -- -='ENHANCD_DISABLE_HYPHEN=1 cd -'
 fi
