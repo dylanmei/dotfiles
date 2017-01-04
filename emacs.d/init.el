829d527023dacebb99eb961b9193e88399417e5e
Dylan Meissner <dylanmei@gmail.com>
2017-01-04 05:27:13 -0800

    Initial emacs.d

---
+++ emacs.d/init.el
@@ -0,0 +1,21 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package use-package
+  :init
+  (setq use-package-verbose t))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
