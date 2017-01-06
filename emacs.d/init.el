147ae52aa85790d560ce77fccd2b464cc7484833
Dylan Meissner <dylanmei@gmail.com>
2017-01-06 06:59:18 -0800

    Organize emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,4 @@
+(package-initialize)
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
@@ -5,17 +6,60 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
-(package-initialize)
+(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+
+;; Don't litter my init file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package use-package
-  :init
-  (setq use-package-verbose t))
+(eval-when-compile
+  (require 'use-package))
 
-(use-package evil
+(defvar backup-dir "~/.emacs.d/backups/")
+(setq backup-directory-alist (list (cons "." backup-dir)))
+(setq make-backup-files nil)
+
+;; Essential settings
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(column-number-mode t)
+
+;; Package-specific configs
+(require 'init-evil)
+(require 'lang-elixir)
+
+(use-package company
   :ensure t
+  :defer t
+  :init
+  (global-company-mode)
   :config
-  (evil-mode 1))
+  (setq company-idle-delay 0.2)
+  (setq company-selection-wrap-around t)
+  (define-key company-active-map [tab] 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+(use-package projectile
+  :ensure t
+  :defer 1
+  :config
+  (projectile-global-mode)
+(setq projectile-enable-caching t))
+
+(use-package undo-tree
+  :ensure t
+  :diminish t
+  :config
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist
+(list (cons "." (expand-file-name "undo-tree-history" user-emacs-directory)))))
+
+(provide 'init)
