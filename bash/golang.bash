4d0a2b4f0c9ffcc3a7c1b2a64eb66fa1f2fb967f
Dylan Meissner <dylanmei@gmail.com>
2018-11-21 22:00:56 -0800

    Update go script

--- bash/golang.bash
+++ bash/golang.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ -d /usr/local/go/bin ]]; then
+  export PATH=$PATH:/usr/local/go/bin
+fi
+
 if [[ -d $HOME/src/go ]]; then
   export GOPATH=$HOME/src/go
   export PATH=$PATH:$GOPATH/bin
