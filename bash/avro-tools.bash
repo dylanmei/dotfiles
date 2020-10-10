d48dcef04a0785e5d4acd97986e7b9a441644b40
Dylan Meissner <dylanmei@gmail.com>
2020-10-10 06:22:05 -0700

    Update avro-tools

--- bash/avro-tools.bash
+++ bash/avro-tools.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if [ -f /opt/avro-tools/avro-tools-1.9.1.jar ]; then
-  alias avro-tools='java -jar /opt/avro-tools/avro-tools-1.9.1.jar'
+AVRO_VERSION=1.9.1
+#AVRO_VERSION=1.8.2
+
+if [ -f /opt/avro-tools/avro-tools-${AVRO_VERSION}.jar ]; then
+  alias avro-tools='java -jar /opt/avro-tools/avro-tools-'$AVRO_VERSION'.jar'
 fi
